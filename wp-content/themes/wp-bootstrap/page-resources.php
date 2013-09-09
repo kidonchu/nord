64a8a75437ce173fd49b41333ad9dab69f26a0ce
kidonchu <kidonchu@gmail.com>
2013-09-08 23:42:18 -0500

    Change Resources page sidebar style

--- wp-content/themes/wp-bootstrap/page-resources.php
+++ wp-content/themes/wp-bootstrap/page-resources.php
@@ -3,10 +3,10 @@
 
 	<div class="content-container with-padding-top with-padding-bottom">
 
-		<div class="row min-height-400">
+		<div class="row">
 
 			<!-- navigation -->
-			<div class="sidebar-container col-md-3 border-right no-padding-right">
+			<div class="sidebar-container col-md-3 no-padding-right">
 
 				<?php include('sidebar-resources.php') ?>
 
