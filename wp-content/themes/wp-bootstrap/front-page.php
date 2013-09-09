2ce636c8f677ed39a0284fd027786f3fcf292464
kidonchu <kidonchu@gmail.com>
2013-09-08 23:32:44 -0500

    Add life on dialysis video

--- wp-content/themes/wp-bootstrap/front-page.php
+++ wp-content/themes/wp-bootstrap/front-page.php
@@ -2,27 +2,40 @@
 
 <div class="container">
 
-	<div class="content-container">
-		<div class="main-feature-container">
-			<div id="myCarousel" class="carousel slide">
-				<ol class="carousel-indicators">
-					<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-					<li data-target="#myCarousel" data-slide-to="1"></li>
-					<li data-target="#myCarousel" data-slide-to="2"></li>
-				</ol>
-					<!-- Carousel items -->
-				<div class="carousel-inner">
-					<div class="active item"><img src="http://lorempixel.com/1000/400/people/1" alt="image0"></div>
-					<div class="item"><img src="http://lorempixel.com/1000/400/people/4" alt="image1"></div>
-					<div class="item"><img src="http://lorempixel.com/1000/400/people/2" alt="image2"></div>
-				</div>
-				<!-- Carousel nav -->
-				<a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
-				<a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+	<div class="main-feature-container">
+<!-- 			<div id="myCarousel" class="carousel slide">
+			<ol class="carousel-indicators">
+				<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+				<li data-target="#myCarousel" data-slide-to="1"></li>
+				<li data-target="#myCarousel" data-slide-to="2"></li>
+			</ol>
+			<div class="carousel-inner">
+				<div class="active item"><img src="http://lorempixel.com/1000/400/people/1" alt="image0"></div>
+				<div class="item"><img src="http://lorempixel.com/1000/400/people/4" alt="image1"></div>
+				<div class="item"><img src="http://lorempixel.com/1000/400/people/2" alt="image2"></div>
+			</div>
+			<a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+			<a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+		</div>
+-->
+		<div class="row">
+			<div class="col-md-8">
+				<iframe width="100%" height="100%" src="//www.youtube.com/embed/M60yzOXTl0Y?rel=0&autoplay=0&autohide=1&modestbranding=1" frameborder="0" allowfullscreen></iframe>
+			</div>
+			<div class="col-md-4">
+				<header class="pageHeader">
+					<h1>NORD</h1>
+				</header>
+				<p>
+					is a nonprofit health agency dedicated to preventing kidney (renal) disease, diabetes and high blood pressure through education and research.
+				</p>
+				<a href="<?php echo home_url('get-involved/donate') ?>" class="btn btn-success">
+					Donate. <br>
+					Prolong and Save Lives
+				</a>
 			</div>
 		</div>
 	</div>
-	<!-- end .content-container -->
 
 	<div class="sub-content-container top-shadow">
 
