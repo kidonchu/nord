b180a85922f365fc410cb0f64d09f3615836b1d6
kidonchu <kidonchu@gmail.com>
2013-09-15 21:52:15 -0500

    Update Donate page

--- wp-content/themes/wp-bootstrap/template-get-involved.php
+++ wp-content/themes/wp-bootstrap/template-get-involved.php
@@ -24,23 +24,16 @@
 				<div class="border-bottom">
 					<ul class="breadcrumb">
 						<li><a href="<?php echo home_url() ?>">Home</a> <span class="divider"></span></li>
-						<li><a href="<?php echo home_url('get-involved') ?>">Get Involved</a> <span class="divider"></span></li>
+						<li>Get Involved <span class="divider"></span></li>
 						<li class="active"><?php the_title() ?></li>
 					</ul>
 				</div>
 
 				<article id="post-<?php the_ID() ?>" role="article" itemscope itemtype="http://schema.org/BlogPosting">
-
-					<header class="pageHeader">
-						<h1 itemprop="headline"><?php the_title() ?></h1>
-					</header>
-
-					<section class="post_content clearfix" itemprop="articleBody">
-
+					<h1 itemprop="headline"><?php the_title() ?></h1>
+					<section class="post-content clearfix" itemprop="articleBody">
 						<?php the_content() ?>
-
 					</section>
-
 				</article>
 
 				<?php endwhile ?>
