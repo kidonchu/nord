a6627ad2fcc3ddead1c5178de03ab467fec73d5a
kidonchu <kidonchu@gmail.com>
2013-11-22 15:39:34 -0600

    Add link to partners page in footer

--- wp-content/themes/nord/footer.php
+++ wp-content/themes/nord/footer.php
@@ -6,6 +6,7 @@
                     <li><a href="<?php echo home_url('get-involved/volunteer') ?>">volunteer</a></li>
                     <li><a href="<?php echo home_url('about-us/contact-us') ?>">contact</a></li>
                     <li>|</li>
+                    <li><a href="<?php echo esc_url(home_url('partners'))?>">partners</a></li>
                     <li><a href="<?php echo get_stylesheet_directory_uri().'/assets/img/brochure.jpg' ?>" target="_blank">brochure</a></li>
                     <li><a href="<?php echo home_url('policy') ?>">policy</a></li>
                 </ul>
