cf1c7240aa7be6ef2e2fecaac295cdfe953c4081
kidonchu <kidonchu@gmail.com>
2013-11-22 16:30:44 -0600

    Fix links in footer

--- wp-content/themes/nord/footer.php
+++ wp-content/themes/nord/footer.php
@@ -1,11 +1,12 @@
         <footer class="footer" role="contentinfo">
             <div class="container">
                 <ul class="footer-nav list-inline">
-                    <li><a href="<?php echo home_url('get-involved/donate') ?>">DONATE</a></li>
-                    <li><a href="<?php echo home_url('get-involved/subscribe') ?>">subscribe</a></li>
-                    <li><a href="<?php echo home_url('get-involved/volunteer') ?>">volunteer</a></li>
-                    <li><a href="<?php echo home_url('about-us/contact-us') ?>">contact</a></li>
+                    <li><a href="<?php echo home_url('donate') ?>">DONATE</a></li>
+                    <li><a href="<?php echo home_url('subscribe') ?>">subscribe</a></li>
+                    <li><a href="<?php echo home_url('volunteer') ?>">volunteer</a></li>
+                    <li><a href="<?php echo home_url('contact-us') ?>">contact</a></li>
                     <li>|</li>
+                    <li><a href="<?php echo esc_url(home_url('e-newsletters'))?>">e-newsletters</a></li>
                     <li><a href="<?php echo esc_url(home_url('partners'))?>">partners</a></li>
                     <li><a href="<?php echo get_stylesheet_directory_uri().'/assets/img/brochure.jpg' ?>" target="_blank">brochure</a></li>
                     <li><a href="<?php echo home_url('policy') ?>">policy</a></li>
@@ -14,7 +15,7 @@
                     <ul class="list-inline social-icon-list">
                         <li class="social-icon-item"><a target="_blank" class="social-icon-link social-icon-facebook" href="http://facebook.com/NORDorg">facebook</a></li>
                         <li class="social-icon-item"><a target="_blank" class="social-icon-link social-icon-twitter" href="http://twitter.com/N_0_R_D">twitter</a></li>
-                        <li class="social-icon-item"><a class="social-icon-link social-icon-subscribe" href="<?php echo home_url('get-involved/subscribe') ?>">subscribe</a></li>
+                        <li class="social-icon-item"><a class="social-icon-link social-icon-subscribe" href="<?php echo home_url('subscribe') ?>">subscribe</a></li>
                     </ul>
                 </div>
                 <p class="copyright">
