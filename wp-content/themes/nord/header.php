61f97729d10f65aa4fdc6eebbdc737ac0955ae1a
kidonchu <kidonchu@gmail.com>
2014-07-22 20:58:15 -0500

    Change header

--- wp-content/themes/nord/header.php
+++ wp-content/themes/nord/header.php
@@ -13,14 +13,15 @@
 <body <?php body_class() ?>>
   
   <!-- Google Tag Manager -->
-<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-MLNGN5"
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-WXF4J2"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-MLNGN5');</script>
+})(window,document,'script','dataLayer','GTM-WXF4J2');</script>
 <!-- End Google Tag Manager -->
+
 
 
     <?php do_action('before') ?>
