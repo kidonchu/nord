28ee5718f6c625ae475b5dc5a11fbb3efab92c73
kidonchu <kidonchu@gmail.com>
2014-08-25 08:09:47 -0500

    Edit category page

--- wp-content/themes/nord/category.php
+++ wp-content/themes/nord/category.php
@@ -1,48 +1,51 @@
 <?php get_header() ?>
+<div class="content">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-8 col-sm-offset-2">
+						<?php if ( have_posts() ) : while ( have_posts() ) : the_post() ?>
 
-	<!-- category.php -->
-	<div class="row">
-		<div class="col-md-3 col-sm-4">
-			<div class="spacer2"></div>
-			<div class="sidebar">
-				<?php if (is_active_sidebar('blog')): ?>
-					<?php dynamic_sidebar('blog') ?>
-				<?php endif ?>
-			</div>
-		</div>
-		<div class="col-md-9 col-sm-8">
-			<div class="spacer2"></div>
+							<article id="post-<?php the_ID() ?>" <?php post_class('summary') ?>>
+                   <div class="entry-content">
+												<div class="summary-date"><?php the_time(get_option('date_format')) ?></div>
+											<h3 class="summary-title">
+												<a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title() ?></a>
+											</h3>
+											<?php the_excerpt() ?>
+									</div>
+							</article><!-- #post-## -->
 
-			<div class="row">
-				<div class="col-md-8">
-					<?php if ( have_posts() ) : while ( have_posts() ) : the_post() ?>
+							<?php endwhile; ?>
 
-						<article id="post-<?php the_ID() ?>" <?php post_class('summary') ?>>
-
-							<div class="summary-date"><?php the_time(get_option('date_format')) ?></div>
-							<h3 class="summary-title">
-								<a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title() ?></a>
-							</h3>
-							<?php the_excerpt() ?>
-
-						</article><!-- #post-## -->
-
-						<?php endwhile; ?>
-
-					<?php endif; ?>
-					<nav role="navigation" class="paging-navigation cf">
-						<h1 class="screen-reader-text"><?php _e( 'Page navigation', 'nord' ); ?></h1>
-						<?php if ( get_next_posts_link() ) : ?>
-							<div class="nav-previous"><?php next_posts_link(__('<span class="meta-nav">&larr;</span> Older posts', 'nord')) ?></div>
 						<?php endif; ?>
+						<nav role="navigation" class="paging-navigation cf">
+							<h1 class="screen-reader-text"><?php _e( 'Page navigation', 'nord' ); ?></h1>
+							<?php if ( get_next_posts_link() ) : ?>
+								<div class="nav-previous"><?php next_posts_link(__('<span class="meta-nav">&larr;</span> Older posts', 'nord')) ?></div>
+							<?php endif; ?>
 
-						<?php if ( get_previous_posts_link() ) : ?>
-							<div class="nav-next"><?php previous_posts_link(__('Newer posts <span class="meta-nav">&rarr;</span>', 'nord')) ?></div>
-						<?php endif; ?>
-					</nav>
+							<?php if ( get_previous_posts_link() ) : ?>
+								<div class="nav-next"><?php previous_posts_link(__('Newer posts <span class="meta-nav">&rarr;</span>', 'nord')) ?></div>
+							<?php endif; ?>
+						</nav>
 				</div>
-			</div>
 		</div>
-	</div>
+</div>
+</div>
 
+<?php get_header() ?>
+<div class="content">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-8 col-sm-offset-2">
+                <article id="post-<?php the_ID() ?>" <?php post_class('entry') ?>>
+                    <h1 class="entry-title text-center"><?php the_title() ?></h1>
+                    <div class="entry-content">
+                        <?php the_content() ?>
+                    </div>
+                </article>
+            </div>
+        </div>
+    </div>
+</div>
 <?php get_footer() ?>
